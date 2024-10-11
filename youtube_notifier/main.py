4e7efcb06e7e3f03905c5276773ea4d2a489e743
Anastasia Russo <russo.anastasia@proton.me>
2024-10-11 17:04:12 +0200

    Removed scheduling logic

--- youtube_notifier/main.py
+++ youtube_notifier/main.py
@@ -1,7 +1,4 @@
-import schedule
-import time
 import logging
-from logging.handlers import RotatingFileHandler
 from youtube_api import check_live_stream
 from email_utils import send_email
 
@@ -16,23 +13,12 @@ def main():
 
 
 if __name__ == '__main__':
-    handler = RotatingFileHandler(
-        "yt-livestream-bot.log",
-        maxBytes=5 * 1024 * 1024,
-        backupCount=5
-    )
-
     logging.basicConfig(
         level=logging.INFO,
         format='%(asctime)s - %(levelname)s - %(message)s',
         handlers=[
-            handler,
             logging.StreamHandler()
         ]
     )
 
-    schedule.every(90).minutes.do(main)
-
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
+    main()
